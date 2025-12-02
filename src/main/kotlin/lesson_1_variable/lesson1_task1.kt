a39a7acfe924f76b12743db0de4b53f32552cd4b
Marina Romanova <m4lanyuk@yandex.ru>
2025-12-02 03:49:12 +0300

    KS-1-3 add variables

---
+++ src/main/kotlin/lesson_1_variable/lesson1_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_1_variable
+
+fun main() {
+    var numberOfOrders = 75
+    val gratitudeMessage = "Спасибо за заказ"
+
+    println(numberOfOrders)
+    println(gratitudeMessage)
+}
